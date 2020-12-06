d002d4139ff5b5939779666f3b0d4175088eef65
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-06 19:04:12 +0200

    feat: add sfml test code
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- Fishie/Program.cs
+++ Fishie/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SFML;
+using SFML.Graphics;
 
 namespace Fishie
 {
@@ -6,7 +11,17 @@ namespace Fishie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RenderWindow window = new RenderWindow(new SFML.Window.VideoMode(200, 200), "SFML");
+            CircleShape cs = new CircleShape(100.0f);
+            cs.FillColor = Color.Green;
+            window.SetActive();
+            while (window.IsOpen)
+            {
+                window.Clear();
+                window.DispatchEvents();
+                window.Draw(cs);
+                window.Display();
+            }
         }
     }
 }
