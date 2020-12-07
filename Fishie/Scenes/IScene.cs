323b4595b068e4e516cd385a57a7fd87d448b1dd
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-07 22:22:22 +0200

    feat(scenes): correct Scene interface
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- Fishie/Scenes/IScene.cs
+++ Fishie/Scenes/IScene.cs
@@ -5,12 +5,13 @@ using System.Text;
 
 namespace Fishie.Scenes
 {
-    public interface IScene
+    public interface IScene : Drawable
     {
-        public void Init();
+        public void OnPush();
+        public void OnPop();
         public void HandleInput();
         public void RegisterEventHandlers(RenderWindow target);
+        public void UnregisterEventHandlers(RenderWindow target);
         public void Update(Double deltaTime);
-        public void Draw(RenderTarget target, RenderStates states);
     }
 }
