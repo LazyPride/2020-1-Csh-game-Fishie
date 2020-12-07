3fc1792f185ec1ed50e86dcd3c3a4cd3c9a63267
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-07 22:07:55 +0200

    feat(scenes): add nonimplemented Menu scene
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ Fishie/Scenes/SceneMenu.cs
@@ -0,0 +1,35 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fishie.Scenes
+{
+    class SceneMenu : IScene
+    {
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterEventHandlers(RenderWindow target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(double deltaTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
