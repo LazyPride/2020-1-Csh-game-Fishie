916048dbbad8e8799b2f14d20883a1321b7a764f
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-07 19:22:49 +0200

    feat(design): add Game's methods
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ Fishie/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fishie
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Run();
+        }
+          
+    }
+}
---
+++ Fishie/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SFML;
+using SFML.Graphics;
+
+namespace Fishie
+{
+    class Game
+    {
+        public void Run()
+        {
+            Init();
+            RegisterEventHandlers();
+            while(true)
+            {
+                // Dispatch events
+                HandleInput();
+                Update();
+                Draw();
+            }
+        }
+
+        private RenderWindow window;
+
+        private void Init()
+        {
+
+        }
+        private void RegisterEventHandlers()
+        {
+
+        }
+        private void HandleInput()
+        {
+
+        }
+        private void Update()
+        {
+
+        }
+        private void Draw()
+        {
+
+        }
+    }
+}
