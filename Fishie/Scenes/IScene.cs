f656da9621e2355daeefc433eba0307f09d55f96
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-07 19:35:48 +0200

    feat(design): add IScene interface
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ Fishie/Scenes/IScene.cs
@@ -0,0 +1,16 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fishie.Scenes
+{
+    interface IScene
+    {
+        public void Init();
+        public void HandleInput();
+        public void RegisterEventHandlers(RenderWindow target);
+        public void Update(Double deltaTime);
+        public void Draw(RenderTarget target, RenderStates states);
+    }
+}
